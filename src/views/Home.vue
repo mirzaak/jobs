761a39db4c9fdf18aace93d7a75f03b39a56f48c
mirzaak <mirzakaric002@gmail.com>
2021-12-04 14:07:01 +0100

    gas

---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <h1>Homepage</h1>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Alias labore, et beatae quos pariatur deserunt eveniet eligendi. Dolor fugiat obcaecati quod facere. Pariatur aut officiis ad in minima reprehenderit vero ullam necessitatibus ex nesciunt exercitationem hic facere odio, ea perferendis fugiat ab est quidem dolor, molestias reiciendis dicta vitae? Placeat, at quasi quae blanditiis repudiandae saepe neque maiores ab officia tenetur labore ipsa possimus. Magnam velit autem vel nam nobis necessitatibus magni, nisi ad dignissimos! Temporibus nesciunt laboriosam, numquam obcaecati beatae, minima ut mollitia dolores, qui cum esse dolorem perferendis aliquid assumenda odio. Minus accusamus adipisci, deserunt similique repellendus inventore!</p>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Alias labore, et beatae quos pariatur deserunt eveniet eligendi. Dolor fugiat obcaecati quod facere. Pariatur aut officiis ad in minima reprehenderit vero ullam necessitatibus ex nesciunt exercitationem hic facere odio, ea perferendis fugiat ab est quidem dolor, molestias reiciendis dicta vitae? Placeat, at quasi quae blanditiis repudiandae saepe neque maiores ab officia tenetur labore ipsa possimus. Magnam velit autem vel nam nobis necessitatibus magni, nisi ad dignissimos! Temporibus nesciunt laboriosam, numquam obcaecati beatae, minima ut mollitia dolores, qui cum esse dolorem perferendis aliquid assumenda odio. Minus accusamus adipisci, deserunt similique repellendus inventore!</p>
+
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'Home',
+  components: { }
+}
+</script>
